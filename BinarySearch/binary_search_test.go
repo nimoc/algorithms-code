0985d22881b6fa6e593836f88f5982916f929bfd
Nimo <nimo.jser@gmail.com>
2020-08-08 18:08:53 +0800

    ReadFileInts

--- BinarySearch/binary_search_test.go
+++ BinarySearch/binary_search_test.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	ge "github.com/og/x/error"
 	gtest "github.com/og/x/test"
@@ -10,11 +8,10 @@ import (
 	"os"
 	"path"
 	"sort"
+	"strconv"
 	"testing"
 )
 
-var newLine = []byte("\n")
-var comma = []byte(`,`)
 func rank(key int, list []int)  int{
 	lo := 0
 	hi := len(list)-1
@@ -31,7 +28,23 @@ func rank(key int, list []int)  int{
 	}
 	return -1
 }
-func TestBasic(t *testing.T) {
+func rank2(key int, list []int)  int{
+	lo := 0
+	hi := len(list)-1
+	for ;lo<=hi; {
+		mid := lo + (hi - lo) / 2
+		switch {
+		case key < list[mid]:
+			hi = mid - 1
+		case key > list[mid]:
+			lo = mid + 1
+		default:
+			return mid
+		}
+	}
+	return -1
+}
+func TestBasic1(t *testing.T) {
 	as := gtest.NewAS(t)
 	whiteList := ReadFileInts("res/data/tinyW.txt")
 	data := ReadFileInts("res/data/tinyT.txt")
@@ -45,14 +58,37 @@ func TestBasic(t *testing.T) {
 	}
 	as.Equal(result, []int{50, 99, 13})
 }
+func TestBasic2(t *testing.T) {
+	as := gtest.NewAS(t)
+	whiteList := ReadFileInts("res/data/tinyW.txt")
+	data := ReadFileInts("res/data/tinyT.txt")
+	sort.Ints(whiteList)
+	var result []int
+	for _, n := range data {
+		if rank2(n, whiteList) < 0 {
+			result = append(result, n)
+			fmt.Println(n)
+		}
+	}
+	as.Equal(result, []int{50, 99, 13})
+}
+
+// 读取文件并将内容转换为 []int
 func ReadFileInts(filename string) (ints []int) {
 	filename = path.Join(ge.String(os.Getwd()), "../", filename)
 	data, err := ioutil.ReadFile(filename) ; check(err)
-	data = bytes.TrimSuffix(data, []byte("\n"))
-	jsonb := []byte("[")
-	jsonb = append(jsonb, bytes.ReplaceAll(data, newLine,  comma)...)
-	jsonb = append(jsonb, []byte("]")...)
-	err = json.Unmarshal(jsonb, &ints) ; check(err)
+	newLine := byte(10)
+	var temp []byte
+	for _, b := range data {
+		if b == newLine {
+			if len(temp) == 0 {continue}
+			i, err := strconv.Atoi(string(temp)) ; check(err)
+			ints = append(ints, i)
+			temp = []byte{}
+			continue
+		}
+		temp = append(temp, b)
+	}
 	return
 }
 func check(err error) {
